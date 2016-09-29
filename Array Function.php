727295b6e9e8f4c50f163f8bee5fc1e3c2ba9e73
Vin57 <vincphil54800@gmail.com>
2016-09-29 20:13:17 +0200

    Add files via upload

---
+++ Array Function.php
@@ -0,0 +1,48 @@
+
+<?php
+
+/*********************************************************************
+/****************************ARRAY FUNCTION*************************/
+/*********************************************************************/
+
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!each function have probably an exemple of how to use it!!!!!!!!!!!!!!!!!!!!!
+!!!!!!Check the "name of the function" in the
+                "ARRAY FUNCTION" section in 
+                "Watch and Test" file in the
+                "How To Use It" folder!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+# If you want to return the key on an associative array to get the first occurence 
+# of this array for exemple and if you don't know the key of the element that you search,
+# it's impossible to find it without create you're own fonction, you can use for example : 
+
+/**
+ * Retourne la premiére clé/valeur d'un tableau (notament utile pour les tableaux associatifs)
+ * @param array $tab un tableau
+ * @param int $mode 0 => renvoie la premiére valeur du tableau, 1=> renvoie la premiére clé du tableau
+ * @return mixed la premiére occurence d'un tableau ou nul si le tableau est vide
+ */
+public static function firstKey($tab,$mode = 0)
+{
+    foreach($tab as $key=>$values)
+    {
+        if(is_array($values))
+        {
+            self::firstKey($values);
+        }
+        if($mode == 0)
+        {
+            return $values;
+        }
+        elseif($mode == 1)
+        {
+            return $key;            
+        }
+    }
+    return null;
+}
+
+
+?>
---
+++ Date Function.php
@@ -0,0 +1,69 @@
+
+<?php
+
+
+/*********************************************************************/
+/****************************DATE FUNCTION*************************/
+/*********************************************************************/
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!each function have probably an exemple of how to use it!!!!!!!!!!!!!!!!!!!!!
+!!!!!!Check the "name of the function" in the
+                "DATE FUNCTION" section in 
+                "Watch and Test" file in the
+                "How To Use It" folder!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+# When you just want to operate to date and it's not work, you just become crazy,
+# This function may be the solution to all your problem with date operation
+
+/**
+     * operationDate : retourne en nombre de jour le résultat d'une opération entre deux dates
+     * @params string : $dateOne La plus ancienne date (au format standard php (YYYY-MM-DD))
+     * @params string : $dateOne La date la plus récente (au format standard php (YYYY-MM-DD))
+     * @params : $operateur l'opérateur (le signe) de l'opération à effectuer
+     * @return : un nombre de jour sous forme d'entier
+     */
+     function operationDate($dateOne,$datetwo,$operateur)
+    {
+        $d1 = date('Y-m-d',strtotime($dateOne));
+        $d2 = date('Y-m-d',strtotime($datetwo));
+        if(
+            checkdate(
+                    (date('m',strtotime($d1))),
+                    (date('d',strtotime($d1))),
+                    (date('Y',strtotime($d1)))
+                    )
+                and 
+            checkdate(
+                    (date('m',strtotime($d2))),
+                    (date('d',strtotime($d2))),
+                    (date('Y',strtotime($d2)))
+                    )
+          )
+        {
+            switch($operateur)
+            {
+                case "-":
+                {
+                    $result = strtotime($d1) - strtotime($d2);
+                    $result = $result/60/60/24;
+                }break;
+                case "+":
+                {
+                    $result = strtotime($d1) + strtotime($d2);
+                    $result = $result/60/60/24;
+                }break;
+                case "*":
+                {
+                    $result = strtotime($d1) * strtotime($d2);
+                    $result = $result/60/60/24;
+                }break;
+            }
+        }
+        else
+        {
+            $result = -1;
+        }
+        return $result;
+    }
+?>
