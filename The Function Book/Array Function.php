043666a082deef8a7e3f2b4aabca5ff75448846a
Vin57 <vincphil54800@gmail.com>
2016-10-05 16:42:39 +0200

    ajout de la fonction displayExceptList

--- The Function Book/Array Function.php
+++ The Function Book/Array Function.php
@@ -48,5 +48,57 @@ public static function firstKey($tab,$mode = 0)
     return null;
 }
 
+------------------------------------------------------------------
+  ------------------------------displayExceptList-------------------------
+------------------------------------------------------------------
+
+#If you want to remove some value of an array : 
+
+/**
+     * Affiche une liste de choix à partir de $tab, en ôtant les valeurs de $tabException
+     * @params string $tab : un tableau de deux colonnes
+     * @params string $tabException : le tableau des valeurs à évitée
+     * @params string $classe : la classe CSS à appliquer à l'élément
+     * @params string $id : l'id (et nom) de la liste de choix
+     * @params int $size : l'attribut size de la liste de choix
+     * @params string $idSelect : l'élément à présélectionner dans la liste
+     * @params string $onchange : le nom de la fonction à appeler 
+     * en cas d'événement onchange()
+    */
+    public static function displayExceptList ($tab,$tabException, $classe, $id, $size, $idSelect, $onchange) {
+        // affichage de la liste de choix
+        echo '<select class="'.$classe.'" id="'.$id.'" name="'.$id.'" size="'
+                .$size.'" onchange="'.$onchange . '">';
+        if (count($tab) && (empty($idSelect))) {
+            $idSelect = $tab[0];
+        }
+        for($i=0;$i<count($tab);$i++) {
+            $evade = 0 ;
+            for($e=0;$e<count($tabException);$e++)
+            {
+                if($tab[$i]==$tabException[$e])
+                {
+                    $evade = 1 ;
+                }
+                if($evade==1)
+                {
+                    $e = count($tabException);
+                }
+            }
+            // l'élément en paramètre est présélectionné
+            if($evade==0)// Alors l'élément doit être intégré à la liste
+            {
+                if ($tab[$i] != $idSelect) {
+                    echo '<option value="'.$tab[$i].'">'.$tab[$i].'</option>';
+                } 
+                else {
+                    echo '<option selected value="'.$tab[$i].'">'.$tab[$i].'</option>';
+                }
+            }
+        }
+        echo('</select>');
+        return ($tab);
+    }
+
 
 ?>
