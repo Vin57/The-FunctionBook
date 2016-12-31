9280c48a4cc03935339791de87c2110a3809db18
Vincent <vincphil54800@gmail.com>
2016-12-31 17:43:16 +0100

    Update Watch and Test.sql

--- SQL book/How To Use It/Watch and Test.sql
+++ SQL book/How To Use It/Watch and Test.sql
@@ -23,3 +23,26 @@ sp_index_exists(IN name_index VARCHAR(255),
                 OUT p_result BOOLEAN)
 /////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////
+-- This procedure has two way of functioning
+------------------FIRST WAY-------------------
+------------------BASIC MODE------------------
+-- This mode allows you to know if the index that you give in first parameter is exist :
+-- (second and third parameter are optional you could set them to NULL)
+-- fourth parameter has to be null or false to not delete the index when he is found
+-- fifth parameter should always be a mysql variable
+CALL sp_index_exists('nom_index','nom_table','nom_base',NULL,@result);
+-- now execute the previous command and get back the value of the @result (fifth parameter)
+SELECT @result;-- 1 => the index exist, 2 => index does not exist
+
+
+------------------SECOND WAY-------------------
+------------------DELETE MODE------------------
+-- This mode delete the index if it has been found
+-- second paramete have to be the name of the table where the index should be
+-- (third parameter are optional you could set them to NULL)
+-- fourth parameter has to be true to delete the index when he is found
+-- fifth parameter should always be a mysql variable
+CALL sp_index_exists('nom_index','nom_table','nom_base',TRUE,@result);
+-- if no exception has been found, and if the index has been found in the specify table,
+-- the index have been removed from the specify table if an error occur, that's may be
+-- that you forgot to specify the second parameter
